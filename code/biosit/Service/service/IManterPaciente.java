b42a33ad58d9a847c66eb20327c49203df76689b
pbarruetavena <pbarruetavenavieira@gmail.com>
2023-10-09 23:49:50 -0300

    Interface do serviço ManterPaciente

---
+++ code/biosit/Service/service/IManterPaciente.java
@@ -0,0 +1,17 @@
+
+import br.cefetmg.biosit.DTO.dto.Paciente;
+import java.util.Date;
+
+/**
+ *
+ * @author Pedro Gabriel
+ */
+public interface IManterPaciente {
+    
+    public String cadastrar(Paciente paciente);
+    public String atualizar(Paciente paciente);
+    public String excluir(Paciente paciente);
+    
+    public Paciente pesquisar(String id, String nome, Date dataNasc);
+    
+}
