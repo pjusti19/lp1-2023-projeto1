dc8cabc1c74efb0978e95d401bc91b91f4de157e
pbarruetavena <pbarruetavenavieira@gmail.com>
2023-10-17 00:53:37 -0300

    Mudei o diretório

---
+++ code/biosit/Service/src/test/java/service/IManterPaciente.java
@@ -0,0 +1,18 @@
+package service;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Pedro Gabriel
+ */
+
+public interface IManterPaciente {;
+    
+    //public String cadastrar(Paciente paciente);
+    //public String atualizar(Paciente paciente);
+    //public String excluir(Paciente paciente);
+    
+    //public Paciente pesquisar(String id, String nome, Date dataNasc);
+    
+}
