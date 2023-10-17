4081042026c5f4b786f41284af36a28a2f6c87e3
pbarruetavena <pbarruetavenavieira@gmail.com>
2023-10-17 00:54:02 -0300

    Mudei o diretório

---
+++ code/biosit/Service/src/test/java/service/implement/ManterPaciente.java
@@ -0,0 +1,32 @@
+package service.implement;
+
+
+import java.util.Date;
+import service.IManterPaciente;
+
+/**
+ *
+ * @author Pedro Gabriel
+ */
+public class ManterPaciente implements IManterPaciente {
+    /*public String cadastrar(Paciente paciente) {
+        String id = "";
+        //verificar os dados do cliente
+        // usar classe DAO para gravar no bd
+        return id;
+    }
+    
+    public String atualizar(Paciente paciente) {
+        String id = "";
+        return id;
+    }
+    public String excluir(Paciente paciente) {
+        String id = "";
+        return id;
+    }
+    
+    public List<Paciente> pesquisar(String id, String nome, Date dataNasc) {
+        List<Paciente> pacientes;
+        return pacientes;
+    }*/
+}
