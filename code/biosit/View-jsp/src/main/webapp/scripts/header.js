4ffbd6475d6eb91e5c64531a3cfb6ef42ebed8ad
paulistaLH <pedrinhojusti@gmail.com>
2023-10-23 21:24:13 -0300

    incrementação na estilização dos botões de menu do header

--- code/biosit/View-jsp/src/main/webapp/scripts/header.js
+++ code/biosit/View-jsp/src/main/webapp/scripts/header.js
@@ -1,30 +1,39 @@
 
-document.querySelector("#icone").addEventListener('click', ()=> location.href="index.jsp");
+document.querySelector("#icone").addEventListener('click', () => location.href = "index.jsp");
 
-opcoesEl = document.querySelector("#opcoes");
+let opcoesEl = document.querySelector("#opcoes");
+let mostrarMenuEl = document.querySelector("#mostrar-menu");
 
-document.querySelector("#mostrar-menu").addEventListener('click', ()=>{
+mostrarMenuEl.addEventListener('click', () => {
     notificacoesEl.style.right = '-1000px';
     notificacoesEl.classList.add('menu-escondido');
     if (opcoesEl.classList.contains('menu-escondido')) {
         opcoesEl.style.right = '0';
         opcoesEl.classList.remove('menu-escondido');
+        mostrarMenuEl.style.background = 'rgb(209, 209, 209)';
+        mostrarNotificacoesEl.style.background = 'white';
     } else {
         opcoesEl.style.right = '-1000px';
         opcoesEl.classList.add('menu-escondido');
+        mostrarMenuEl.style.background = 'white';
     }
 });
 
-notificacoesEl = document.querySelector("#notificacoes");
+let notificacoesEl = document.querySelector("#notificacoes");
+let mostrarNotificacoesEl = document.querySelector("#mostrar-notificacoes");
 
-document.querySelector("#mostrar-notificacoes").addEventListener('click', ()=>{
+mostrarNotificacoesEl.addEventListener('click', () => {
     opcoesEl.style.right = '-1000px';
     opcoesEl.classList.add('menu-escondido');
     if (notificacoesEl.classList.contains('menu-escondido')) {
         notificacoesEl.style.right = '0';
         notificacoesEl.classList.remove('menu-escondido');
+        mostrarNotificacoesEl.style.background = 'rgb(209, 209, 209)';
+        mostrarMenuEl.style.background = 'white';
+
     } else {
         notificacoesEl.style.right = '-1000px';
         notificacoesEl.classList.add('menu-escondido');
+        mostrarNotificacoesEl.style.background = 'white';
     }
 });
