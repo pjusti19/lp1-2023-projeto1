6d4966b3eaf73e251191ef3a1c33fa8803e7b98c
duda-abecassis <duda.abecassis@gmail.com>
2023-11-06 21:51:11 -0300

    Criação da página jsp e das classes de serviço e controle dos exames.

---
+++ code/biosit/Service/src/main/java/br/cefetmg/biosit/service/IRequisitarExame.java
@@ -0,0 +1,18 @@
+
+package br.cefetmg.biosit.service;
+
+import br.cefetmg.biosit.dto.Exame;
+import br.cefetmg.biosit.dto.exception.*;
+import java.util.Date;
+import java.util.List;
+
+
+public interface IRequisitarExame {
+    
+    public String cadastrar(Exame exame) throws CadastroException;
+    public String atualizar(Exame exame) throws Exception;
+    public String excluir(Exame exame) throws Exception;
+    
+    public List<Exame> pesquisar(Exame exame) throws Exception;
+    
+}
