035540d3eaaef236d7ac312c63dc51da2956d9bb
David Matos Caldeira Eller <109438700+Dranork@users.noreply.github.com>
2023-11-13 09:25:20 -0300

    Add files via upload

--- code/biosit/DTO/src/main/java/br/cefetmg/biosit/dto/Usuario.java
+++ code/biosit/DTO/src/main/java/br/cefetmg/biosit/dto/Usuario.java
@@ -4,6 +4,7 @@ public class Usuario {
     private int idUsuario;
     private String nomeUsuario;
     private String senhaUsuario;
+    private String tipoUsuario;
 
     public int getIdUsuario() {
         return idUsuario;
@@ -27,6 +28,14 @@ public class Usuario {
 
     public void setSenhaUsuario(String senhaUsuario) {
         this.senhaUsuario = senhaUsuario;
+    }
+    
+    public String getTipoUsuario () {
+        return tipoUsuario;
+    }
+    
+    public void setTipoUsuario (String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
     }
 }
 
