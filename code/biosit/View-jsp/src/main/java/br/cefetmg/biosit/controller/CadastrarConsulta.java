a5cb8a87feb4c4fe605c9c3a36cfd6db5d9cf433
paulistaLH <pedrinhojusti@gmail.com>
2023-12-04 20:06:55 -0300

    try-catch

--- code/biosit/View-jsp/src/main/java/br/cefetmg/biosit/controller/CadastrarConsulta.java
+++ code/biosit/View-jsp/src/main/java/br/cefetmg/biosit/controller/CadastrarConsulta.java
@@ -17,18 +17,17 @@ import br.cefetmg.biosit.service.implement.ManterPaciente;
 import br.cefetmg.biosit.service.implement.ManterProfissional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Aluno
  */
-
-
 // ISSO DAQUI TA MUITO FEIO, MAS FUNCIONA!
-
 public class CadastrarConsulta {
 
-    public static String execute(HttpServletRequest request) throws Exception {
+    public static String execute(HttpServletRequest request) {
         String jsp = "/consulta.jsp";
 
         try {
@@ -49,48 +48,71 @@ public class CadastrarConsulta {
 
             IAgendarConsulta agendarConsulta = new AgendarConsulta();
             agendarConsulta.cadastrar(consulta);
-            
+
             List<Profissional> profissionais = new ArrayList<>();
             List<Paciente> pacientes = new ArrayList<Paciente>();
             Paciente paciente = new Paciente(null, null, null, null);
-            
+
             ManterPaciente manterPaciente = new ManterPaciente();
             pacientes = manterPaciente.pesquisar(paciente);
             ManterProfissional manterProfissional = new ManterProfissional();
             profissionais = manterProfissional.pesquisarTodos();
-            
+
             request.setAttribute("pacientes", pacientes);
             request.setAttribute("profissionais", profissionais);
-            
-            request.setAttribute("sucess", "Consulta cadastrada com sucesso");
-        } catch(MedicoIndisponivelException | CadastroException e) {
-            List<Profissional> profissionais = new ArrayList<>();
-            List<Paciente> pacientes = new ArrayList<Paciente>();
-            Paciente paciente = new Paciente(null, null, null, null);
-            
-            ManterPaciente manterPaciente = new ManterPaciente();
-            pacientes = manterPaciente.pesquisar(paciente);
-            ManterProfissional manterProfissional = new ManterProfissional();
-            profissionais = manterProfissional.pesquisarTodos();
-            
-            request.setAttribute("pacientes", pacientes);
-            request.setAttribute("profissionais", profissionais);
-            request.setAttribute("tperror", "cadastrarConsulta");
-            request.setAttribute("error", e.getMessage());    
+
+            request.setAttribute("success", "Consulta cadastrada com sucesso");
+        } catch (MedicoIndisponivelException e) {
+            try {
+                List<Profissional> profissionais = new ArrayList<>();
+                List<Paciente> pacientes = new ArrayList<Paciente>();
+                Paciente paciente = new Paciente(null, null, null, null);
+                ManterPaciente manterPaciente = new ManterPaciente();
+                pacientes = manterPaciente.pesquisar(paciente);
+                ManterProfissional manterProfissional = new ManterProfissional();
+                profissionais = manterProfissional.pesquisarTodos();
+                request.setAttribute("pacientes", pacientes);
+                request.setAttribute("profissionais", profissionais);
+                
+                request.setAttribute("tperror", "cadastrarConsulta");
+                request.setAttribute("error", e.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(CadastrarConsulta.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (CadastroException e) {
+            try {
+                List<Profissional> profissionais = new ArrayList<>();
+                List<Paciente> pacientes = new ArrayList<Paciente>();
+                Paciente paciente = new Paciente(null, null, null, null);
+                ManterPaciente manterPaciente = new ManterPaciente();
+                pacientes = manterPaciente.pesquisar(paciente);
+                ManterProfissional manterProfissional = new ManterProfissional();
+                profissionais = manterProfissional.pesquisarTodos();
+                request.setAttribute("pacientes", pacientes);
+                request.setAttribute("profissionais", profissionais);
+                
+                request.setAttribute("tperror", "cadastrarConsulta");
+                request.setAttribute("error", e.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(CadastrarConsulta.class.getName()).log(Level.SEVERE, null, ex);
+            }
         } catch (Exception e) {
-            List<Profissional> profissionais = new ArrayList<>();
-            List<Paciente> pacientes = new ArrayList<Paciente>();
-            Paciente paciente = new Paciente(null, null, null, null);
-            
-            ManterPaciente manterPaciente = new ManterPaciente();
-            pacientes = manterPaciente.pesquisar(paciente);
-            ManterProfissional manterProfissional = new ManterProfissional();
-            profissionais = manterProfissional.pesquisarTodos();
-            
-            request.setAttribute("pacientes", pacientes);
-            request.setAttribute("profissionais", profissionais);
-            request.setAttribute("tperror", "cadastrarConsulta");
-            request.setAttribute("error", "Não foi possível realizar o cadastro, tente novamente");
+            try {
+                List<Profissional> profissionais = new ArrayList<>();
+                List<Paciente> pacientes = new ArrayList<Paciente>();
+                Paciente paciente = new Paciente(null, null, null, null);
+                ManterPaciente manterPaciente = new ManterPaciente();
+                pacientes = manterPaciente.pesquisar(paciente);
+                ManterProfissional manterProfissional = new ManterProfissional();
+                profissionais = manterProfissional.pesquisarTodos();
+                request.setAttribute("pacientes", pacientes);
+                request.setAttribute("profissionais", profissionais);
+                
+                request.setAttribute("tperror", "cadastrarConsulta");
+                request.setAttribute("error", "Não foi possível realizar o cadastro, tente novamente");
+            } catch (Exception ex) {
+                Logger.getLogger(CadastrarConsulta.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
 
         return jsp;
