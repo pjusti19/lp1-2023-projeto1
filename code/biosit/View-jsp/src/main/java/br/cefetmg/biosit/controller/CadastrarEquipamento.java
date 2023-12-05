bac2980aac8b81b0f8b86edb7c0441fa69ca1b46
duda-abecassis <duda.abecassis@gmail.com>
2023-12-05 07:11:04 -0300

    Modificações no medicamento

--- code/biosit/View-jsp/src/main/java/br/cefetmg/biosit/controller/CadastrarEquipamento.java
+++ code/biosit/View-jsp/src/main/java/br/cefetmg/biosit/controller/CadastrarEquipamento.java
@@ -1,7 +1,5 @@
 package br.cefetmg.biosit.controller;
 
-import br.cefetmg.biosit.dto.*;
-import br.cefetmg.biosit.service.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -40,25 +38,4 @@ public class CadastrarEquipamento extends HttpServlet {
         
         return jsp;
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
